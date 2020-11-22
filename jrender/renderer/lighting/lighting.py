6c69722289181d815ab4e32b8d4c585ee015666e
zhouwy19 <zhouwy1998@foxmail.com>
2020-11-22 21:02:10 +0800

    add more material

--- jrender/renderer/lighting/lighting.py
+++ jrender/renderer/lighting/lighting.py
@@ -4,6 +4,51 @@ from jittor import nn
 
 from . import *
 
+def lighting(faces, textures, intensity_ambient=0.5, intensity_directional=0.5,
+             color_ambient=(1, 1, 1), color_directional=(1, 1, 1), direction=(0, 1, 0)):
+
+    bs, nf = faces.shape[:2]
+
+    # arguments
+    # make sure to convert all inputs to float tensors
+    color_ambient = jt.array(color_ambient, "float32")
+    color_directional = jt.array(color_directional, "float32")
+    direction = jt.array(direction, "float32")
+
+    if len(color_ambient.shape) == 1:
+        color_ambient = color_ambient.unsqueeze(0)
+    if len(color_directional.shape) == 1:
+        color_directional = color_directional.unsqueeze(0)
+    if len(direction.shape) == 1:
+        direction = direction.unsqueeze(0)
+
+    # create light
+    light = jt.zeros((bs, nf, 3), "float32")
+
+    # ambient light
+    if intensity_ambient != 0:
+        light += intensity_ambient * color_ambient.unsqueeze(1)
+
+    # directional light
+    if intensity_directional != 0:
+        faces = faces.reshape((bs * nf, 3, 3))
+        v10 = faces[:, 0] - faces[:, 1]
+        v12 = faces[:, 2] - faces[:, 1]
+
+        normals = jt.normalize(jt.cross(v10, v12), eps=1e-5)
+        normals = normals.reshape((bs, nf, 3))
+
+        if len(direction.shape) == 2:
+            direction = direction.unsqueeze(1)
+        cos = nn.relu(jt.sum(normals * direction, dim=2))
+        # may have to verify that the next line is correct
+        light += intensity_directional * (color_directional.unsqueeze(1) * cos.unsqueeze(2))
+    # apply
+    light = light.unsqueeze(-2).unsqueeze(-2).unsqueeze(-2)
+    textures *= light
+    return textures
+
+
 class AmbientLighting(nn.Module):
     def __init__(self, light_intensity=0.5, light_color=(1,1,1)):
         super(AmbientLighting, self).__init__()
@@ -24,10 +69,10 @@ class DirectionalLighting(nn.Module):
         self.light_color = light_color
         self.light_direction = light_direction
 
-    def execute(self, light, normals):
-        return directional_lighting(light, normals,
+    def execute(self, diffuseLight, specularLight, normals, positions=None, eye=None):
+        return directional_lighting(diffuseLight, specularLight, normals,
                                         self.light_intensity, self.light_color, 
-                                        self.light_direction)
+                                        self.light_direction, positions, eye)
 
 
 class Lighting(nn.Module):
@@ -48,20 +93,20 @@ class Lighting(nn.Module):
 
     def execute(self, mesh, eyes=None):
         if self.light_mode == 'surface':
-            light = jt.zeros(mesh.faces.shape)
-            light = self.ambient(light)
+            diffuseLight = jt.zeros(mesh.faces.shape)
+            specularLight = jt.zeros(mesh.faces.shape)
+            diffuseLight = 0.5 * self.ambient(diffuseLight)
             for directional in self.directionals:
-                light = directional(light, mesh.surface_normals)
-            if len(mesh.textures.shape) == 4:
-                mesh.textures = mesh.textures * light.unsqueeze(2)
-            elif len(mesh.textures.shape) == 6:
-                mesh.textures = mesh.textures * light.unsqueeze(2).unsqueeze(2).unsqueeze(2)
+                [diffuseLight, specularLight] = directional(diffuseLight, specularLight, mesh.surface_normals, (jt.sum(mesh.face_vertices, dim=2) / 3.0), eyes)
+            mesh.textures = jt.clamp(mesh.textures * diffuseLight.unsqueeze(2) + jt.ones_like(mesh.textures) * specularLight.unsqueeze(2),0.0, 1.0) 
+        
 
         elif self.light_mode == 'vertex':
-            light = jt.zeros(mesh.vertices.shape)
-            light = self.ambient(light)
+            diffuseLight = jt.zeros(mesh.vertices.shape)
+            specualrLight = jt.zeros(mesh.vertices.shape)
+            diffuseLight = self.ambient(diffuseLight)
             for directional in self.directionals:
-                light = directional(light, mesh.vertex_normals)
-            mesh.textures = mesh.textures * light
+                [diffuseLight, specularLight] = directional(diffuseLight, specularLight, mesh.surface_normals, (jt.sum(mesh.face_vertices, dim=2) / 3.0), eyes)
+            mesh.textures = mesh.textures * diffuseLight
 
         return mesh

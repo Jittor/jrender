589d9b91732c6913ee68a1e0f0eb508f69bb96d9
xczz1118 <2574450772@qq.com>
2022-11-17 16:23:46 +0800

    add join_meshes_as_scene && fix some bugs

--- jrender/render2/utils/__init__.py
+++ jrender/render2/utils/__init__.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 from .cuda import *
 from .Cook_Torrance import *
 from .get_points_from_angle import *
-=======
-from .cuda import *
-from .Cook_Torrance import *
->>>>>>> 5fbc0ef13b78afa7b9945513b1b5fd5e2980a95a
